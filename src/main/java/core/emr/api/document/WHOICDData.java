7b6d27f8ffa4f63dd53605b5283767769c642f51
WaiYanHain <waiyanhain215@gmail.com>
2024-04-25 17:32:27 +0630

    w

--- src/main/java/core/emr/api/document/WHOICDData.java
+++ src/main/java/core/emr/api/document/WHOICDData.java
@@ -2,11 +2,13 @@ package core.emr.api.document;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import org.springframework.data.mongodb.core.annotation.Collation;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Document("whoDiseasesCode")
+@Collation
 public class WHOICDData {
     private String code;
     private String descEng;
